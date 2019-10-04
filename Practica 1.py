f1c994273415f8d4559fd466f51027b5a8c03a18
javivi595 <javivi595@hotmail.com>
2019-10-04 20:05:18 +0200

    Update Practica 1.py

--- Practica 1.py
+++ Practica 1.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
+#PROGRAMA QUE CALCULA EL AREA DE UN TRIANGULO CON LA BASE Y LA ALTURA INTRODUCIDOS POR TECLADO
 
 a, b = map(int, input('Escribe base y altura del triangulo separado por espacio: \n').strip().split())
-print(b*a/2)
+print("El area del triangulo es {}".format(b*a/2))
 
 
-# In[13]:
+#Programa que cambia la temperatura de grados celsius a fahrenheit con introducción por teclado
 
 
 c= int(input('Introduce una temperatura en grados celsius:'))
 print("La temperatura en grados fahrenheit es {:.2f}".format(c*(9/5)+32))
 
 
-# In[14]:
+#PROGRAMA QUE CLASIFICA EL NUMERO INTRODUCIDO EN PAR O INPAR
 
 
 n=int(input("Introduce un numero para saber si es par: "))
@@ -25,14 +21,13 @@ else:
     print("El numero {} es inpar".format(n))
 
 
-# In[15]:
+#PROGRAMA QUE IMPRIME CUAL DE LOS DOS NUMEROS INTRODUCIDOS ES EL MÁS GRANDE
 
 
 a, b= map(int, input('Escribe dos numeros para comparar cual es mayor: ').strip().split())
 print ("El numero mayor es {}".format(max(a,b)))
 
 
-# In[ ]:
 
 
 
